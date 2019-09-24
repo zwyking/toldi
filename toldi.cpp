334b89429a03c10337bf540342a2b5c86e2cef93
2603349035 <2603349035@qq.com>
2019-09-24 14:55:19 +0800

    Create toldi.cpp

---
+++ toldi.cpp
@@ -0,0 +1,94 @@
+#include <iostream>//标准C++库中的输入输出类相关头文件。  
+#include <vector>
+#include <pcl/io/io.h>  
+#include <pcl/io/pcd_io.h>//pcd 读写类相关的头文件。  
+#include <pcl/io/ply_io.h>  
+#include <pcl/point_types.h> //PCL中支持的点类型头文件。 
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/features/3dsc.h>
+#include <pcl/features/impl/3dsc.hpp>
+#include <pcl/features/normal_3d.h>
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <Eigen/Dense>
+
+#include <vtkAutoInit.h>
+VTK_MODULE_INIT(vtkRenderingOpenGL);
+
+using namespace std;
+using namespace Eigen;
+using namespace Eigen::internal;
+using namespace Eigen::Architecture;
+
+using PoinT = pcl::PointXYZ;
+using PointN = pcl::Normal;
+float radius = 0.03f;
+
+
+
+int main()
+{
+	/***读取pcd文件***/
+	pcl::PointCloud<PoinT>::Ptr cloud(new pcl::PointCloud<PoinT>);
+	if (pcl::io::loadPCDFile("bunny.pcd", *cloud) == -1)
+	{
+		PCL_ERROR("Couldn't read the pcd file \n"); //文件不存在时，返回错误，终止程序。
+
+		system("pause");
+
+		return (-1);
+	}
+	cout << cloud->size()<<endl;
+
+	/***降采样***/
+	// 创建滤波对象
+	pcl::VoxelGrid<PoinT> filter;
+	filter.setInputCloud(cloud);
+	// 设置体素栅格的大小
+	filter.setLeafSize(0.005f, 0.005f, 0.005f);
+	filter.filter(*cloud);
+	cout << cloud->size() << endl;
+
+
+	/***点云可视化***/
+	pcl::visualization::PCLVisualizer viewer("cloud");
+	viewer.addPointCloud<PoinT>(cloud, "cloud");
+	viewer.spin();
+
+	/***建立kd树，计算点云中的LPR***/
+	//计算法向量(z轴)
+	pcl::NormalEstimation<PoinT, PointN> normal_estimation;
+	normal_estimation.setInputCloud(cloud);
+	pcl::search::KdTree<PoinT>::Ptr tree(new pcl::search::KdTree<PoinT>);
+	normal_estimation.setSearchMethod(tree);
+	pcl::PointCloud<PointN>::Ptr cloud_normals(new pcl::PointCloud<PointN>);
+	normal_estimation.setRadiusSearch(radius);
+	normal_estimation.compute(*cloud_normals);
+
+	//构建kd搜索树
+	pcl::KdTreeFLANN<PoinT> kdtree;
+	kdtree.setInputCloud(cloud);
+
+	vector<int> pointidxRadiusSearch;
+	vector<float> pointRadiusSquaredDistance;
+	vector<Vector3f> pointToKeypointVector;
+
+	//计算x轴
+	for (size_t i = 0; i < cloud->size(); i++)
+	{
+		if (kdtree.radiusSearch(cloud->points[i], radius, pointidxRadiusSearch, pointRadiusSquaredDistance) > 0)
+		{
+			for (size_t j = 0; j < pointidxRadiusSearch.size(); j++)
+			{
+				哈哈哈
+			}
+		}
+	}
+
+
+	
+
+	system("pause");
+
+	return(0);
+}
